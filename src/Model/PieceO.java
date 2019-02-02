28a7945b1b20da48395c4c21293cee2b04de90eb
athai <athai@DESKTOP-3GFC3VT>
2019-02-02 11:22:13 -0700

    Added Piece Logic

---
+++ src/Model/PieceO.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class PieceO extends Piece {
+
+	public PieceO() {
+		type = "O";
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
